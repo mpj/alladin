9d1ee4370e7d3b5ab40fbd5fc6fb9683d32af5ef
Mattias Petter Johansson <mpj@spotify.com>
2015-04-11 20:49:23 +0200

    minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import _ from 'highland'
 import inspector from './utils/inspector-stream'
 
-
-let errorThrower = (err) => {
-  throw err
-}
-
 let constructor = (mongo) => ({
-  pusher: () =>
-    _.pipeline(_.map((doc) => ({
+  pusher: () =>_.pipeline(
+    _.map((doc) => ({
       method: 'insert',
       doc
     })),
     mongo(),
-    _.map((x) => true)),
+    _.map((x) => true)
+  ),
 
   read: (selector) =>
     _([selector])
