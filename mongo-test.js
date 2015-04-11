b8d3fee4748495dbddec56f82b5e521f04b33745
Mattias Petter Johansson <mpj@spotify.com>
2015-04-11 11:41:43 +0200

    streamify mongo facade

--- mongo-test.js
+++ mongo-test.js
@@ -3,26 +3,29 @@ import mongodb from 'mongodb'
 import mongo from './mongo'
 import assert from 'assert'
 let client = Promise.promisifyAll(mongodb.MongoClient)
-
+import _ from 'highland'
 
 
 describe('mongo facade', () => {
   it('hello', (done) => {
-    mongo({
+    let cmd = {
       server: 'mongodb://localhost:27017/test-unit',
       method: 'insert',
       collection: 'stuff',
       doc: { waffles: 3 },
       opts: { w: 1, j: 1 }
-    }).then(function() {
-      client.connectAsync('mongodb://localhost:27017/test-unit').then((db) => {
-        let coll = db.collection('stuff')
-        Promise.promisifyAll(coll)
-        coll.findAsync({waffles:3}).then(function(result) {
-          assert(result)
-          done()
-        }).done()
+    }
+    _([cmd])
+      .through(mongo())
+      .each(function() {
+        client.connectAsync('mongodb://localhost:27017/test-unit').then((db) => {
+          let coll = db.collection('stuff')
+          Promise.promisifyAll(coll)
+          coll.findAsync({waffles:3}).then(function(result) {
+            assert(result)
+            done()
+          }).done()
+        })
       })
-    })
   })
 })
--- mongo.js
+++ mongo.js
@@ -1,27 +1,37 @@
 import Promise from 'bluebird'
 import mongodb from 'mongodb'
+import _ from 'highland'
 
+let client = mongodb.MongoClient;
+let throwAny = (x) => { throw x; }
 
-let client = Promise.promisifyAll(mongodb.MongoClient)
+let fn = () => {
+  return _.flatMap((cmd) => {
+    if (!cmd) throw new Error('command missing.');
+    if (!cmd.server)     throw new Error('server property missing.');
+    if (!cmd.method)     throw new Error('method property missing.');
+    if (!cmd.collection) throw new Error('collection property missing.');
+    if (!cmd.opts)       throw new Error('opts property missing.');
 
+    let out = _()
 
+    return _.wrapCallback(client.connect.bind(client))(cmd.server)
+      .flatMap((db) => {
+        let coll = db.collection(cmd.collection)
+        switch(cmd.method) {
+          case 'insert':
+            if (!cmd.doc) throw new Error('doc property missing.');
+            let insert = _.wrapCallback(coll.insert.bind(coll))
+            return insert(cmd.doc, cmd.opts);
+        }
+      })
+      .errors(throwAny)
 
-let fn = (cmd) => {
-  if (!cmd) throw new Error('command missing.');
-  if (!cmd.server)     throw new Error('server property missing.');
-  if (!cmd.method)     throw new Error('method property missing.');
-  if (!cmd.collection) throw new Error('collection property missing.');
-  if (!cmd.opts)       throw new Error('opts property missing.');
+    return out;
 
-  return client.connectAsync(cmd.server).then((db) => {
-    let coll = db.collection(cmd.collection)
-    Promise.promisifyAll(coll)
-    switch(cmd.method) {
-      case 'insert':
-        if (!cmd.doc) throw new Error('doc property missing.');
-        return coll.insertAsync(cmd.doc, cmd.opts);
-    }
   })
+
+
 }
 
 export default fn
